e0a6291cb1a12fb1e675017dfe02a02f888dce81
Max <avtobots1235@gmail.com>
2021-03-05 22:43:56 +0200

    HW100

--- HW10/JS/script.js
+++ HW10/JS/script.js
@@ -1,24 +1,27 @@
  function calc(initVal) {
    return {
-      add: function(b){
-         return initVal+b;
+      add: function (b){
+         initVal+=b;
       },
-      sub: function(b){
-         return  initVal-b;
+      sub: function (b){
+           initVal-=b;
       },
-      div: function(b){
-         return initVal/b;
+      div: function (b){
+          initVal/=b;
       },
-      mult: function(b){
-         return initVal*b;
+      mult: function (b){
+          initVal*=b;
+      },
+      getResult:function(){
+         return initVal;
       }
    };
-}
+};
 
 const first = calc(4);
 
-console.log(first);
-console.log(first.add(33));
-console.log(first.sub(12));
-console.log(first.div(2));
-console.log(first.mult(4));
+first.add(33);
+first.sub(12);
+first.div(2);
+first.mult(4);
+console.log(first.getResult());
